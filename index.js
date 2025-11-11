7e34bc325e48fd14e0d004a2124c2e35290492bd
usman yaseen <usmanyaseen460.com>
2025-11-11 20:38:27 +0500

    Updated CartItems component and fixed build

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const jwt = require("jsonwebtoken");
 const fs = require("fs");
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // ===== Middleware =====
 app.use(express.json());
